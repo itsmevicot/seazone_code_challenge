5db657ed0a8a4a1f3fb453ae8b11fd1c0c489e88
Victor <victorchagasms@gmail.com>
2023-09-17 01:45:28 -0300

    fix: função para que o django saiba lidar com campos dinâmicos personalizados

--- base/custom_fields.py
+++ base/custom_fields.py
@@ -10,12 +10,36 @@ class CodigoField(models.CharField):
         super().__init__(*args, **kwargs)
 
     def gerar_codigo(self):
-        """ Gera um código alfanumérico de 12 dígitos precedido de um prefixo."""
+        """
+
+        Gera um código alfanumérico de 12 dígitos precedido de um prefixo.
+
+        """
         return self.prefix + ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
 
+    def deconstruct(self):
+        """
+        Desconstrói o campo personalizado para as migrações do Django.
+
+        Este método é necessário para que as migrações do Django saibam como lidar
+        com campos personalizados.
+
+        A principal personalização aqui é para excluir o argumento 'max_length' (que é
+        computado com base no prefixo) e adicionar o 'prefix' ao dicionário kwargs.
+
+        """
+        name, path, args, kwargs = super().deconstruct()
+        if 'max_length' in kwargs:
+            del kwargs['max_length']
+        kwargs['prefix'] = self.prefix
+        return name, path, args, kwargs
+
     def pre_save(self, model_instance, add):
-        """ Realiza o pré-save para evitar códigos iguais e verifica a instância correta da model que utiliza a função
-        para gerar o código. """
+        """
+        Realiza o pré-save para evitar códigos iguais e verifica a instância correta da model que utiliza a função
+        para gerar o código.
+
+        """
         valor = getattr(model_instance, self.attname)
         if not valor:
             while True:
