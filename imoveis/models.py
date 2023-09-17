b3347b6da86ea17b4ce9999003f140c4ac23dd5b
Victor <victorchagasms@gmail.com>
2023-09-17 17:15:38 -0300

    fix: altera DateTimeField da data_ativacao para DateField

--- imoveis/models.py
+++ imoveis/models.py
@@ -4,12 +4,18 @@ from base.custom_fields import CodigoField
 
 
 class Imovel(BaseModel):
+
+    SIM_NAO_CHOICES = (
+        (True, 'Sim'),
+        (False, 'Não'),
+    )
+
     codigo_imovel = CodigoField(prefix='IMO-', primary_key=True, editable=False)
     limite_hospedes = models.IntegerField(default=1)
     quantidade_banheiros = models.IntegerField(default=0)
     aceita_animal_estimacao = models.BooleanField(default=False)
     valor_limpeza = models.FloatField(default=0)
-    data_ativacao = models.DateTimeField()
+    data_ativacao = models.DateField()
 
     def __str__(self):
         return self.codigo_imovel
