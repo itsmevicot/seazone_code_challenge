6d1a451601ba428927167a881d7428e2c842bcdb
Victor <victorchagasms@gmail.com>
2023-09-18 14:00:19 -0300

    fix: correção nos testes e adicionado teste de reserva em data já reservada

--- reservas/tests.py
+++ reservas/tests.py
@@ -24,8 +24,8 @@ class ReservaViewTestCase(APITestCase):
 
         self.reserva = Reserva.objects.create(
             anuncio=self.anuncio,
-            data_checkin=date.today(),
-            data_checkout=date.today() + timedelta(days=5),
+            data_checkin=date(2023, 9, 10),
+            data_checkout=date(2023, 9, 15),
             preco_total=500.0,
             comentario="Bom, bonito e barato!",
             numero_hospedes=3
@@ -51,14 +51,14 @@ class ReservaViewTestCase(APITestCase):
         self.assertEqual(len(response.data), 1)
 
     def teste_pegar_reserva_pelo_id(self):
-        response = self.client.get(reverse('reservas:reservas-detail', args=[self.reserva.codigo_reserva]))
-        reserva = Reserva.objects.get(pk=self.reserva.codigo_reserva)
+        response = self.client.get(reverse('reservas:reservas-detail', args=[self.reserva.pk]))
+        reserva = Reserva.objects.get(codigo_reserva=self.reserva.codigo_reserva)
         serializer = ReservaSerializer(reserva)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def teste_deletar_reserva(self):
-        response = self.client.delete(reverse('reservas:reservas-detail', args=[self.reserva.codigo_reserva]))
+        response = self.client.delete(reverse('reservas:reservas-detail', args=[self.reserva.pk]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def teste_filtrar_reserva_pelo_intervalo_de_datas(self):
@@ -76,3 +76,29 @@ class ReservaViewTestCase(APITestCase):
         response = self.client.patch(reverse('reservas:reservas-detail', args=[self.reserva.codigo_reserva]),
                                      data=update_payload)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def teste_reserva_sobreposta(self):
+        reserva_data = {
+            "anuncio": self.anuncio.id,
+            "data_checkin": "2023-09-18",
+            "data_checkout": "2023-09-25",
+            "preco_total": "300.00",
+            "comentario": "Barato!",
+            "numero_hospedes": 1,
+        }
+        response = self.client.post(reverse('reservas:reservas-list'), reserva_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        reserva_sobreposta_data = {
+            "anuncio": self.anuncio.id,
+            "data_checkin": "2023-09-20",
+            "data_checkout": "2023-09-22",
+            "preco_total": "200.00",
+            "comentario": "Incrível!",
+            "numero_hospedes": 1,
+        }
+        response_sobreposta = self.client.post(reverse('reservas:reservas-list'), reserva_sobreposta_data, format='json')
+
+        self.assertEqual(response_sobreposta.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('data_checkin', response_sobreposta.data)
+        self.assertIn('data_checkout', response_sobreposta.data)
