a729e60ed3f0d4dace79ac68da96c488e0adca31
Victor <victorchagasms@gmail.com>
2023-09-20 16:43:55 -0300

    fix: correção no tipo esperado da pk na url

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -5,5 +5,5 @@ app_name = 'imoveis'
 
 urlpatterns = [
     path('imoveis/', views.ImovelList.as_view(), name='imoveis-list'),
-    path('imoveis/<str:pk>/', views.ImovelDetail.as_view(), name='imoveis-detail'),
+    path('imoveis/<int:pk>/', views.ImovelDetail.as_view(), name='imoveis-detail'),
 ]
--- reservas/urls.py
+++ reservas/urls.py
@@ -5,5 +5,5 @@ app_name = 'reservas'
 
 urlpatterns = [
     path('reservas/', views.ReservaList.as_view(), name='reservas-list'),
-    path('reservas/<str:pk>/', views.ReservaDetail.as_view(), name='reservas-detail'),
+    path('reservas/<int:pk>/', views.ReservaDetail.as_view(), name='reservas-detail'),
 ]
