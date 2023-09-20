cdde18c696ed8669a902774638b33de6fe239ee3
Victor <victorchagasms@gmail.com>
2023-09-20 00:27:33 -0300

    feat: mudança no AtivoManager para permitir outros argumentos em ativos() e inativos()

--- base/manager.py
+++ base/manager.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class AtivoManager(models.Manager):
-    def ativos(self):
-        return self.model.objects.filter(ativo=True)
+    def ativos(self, **kwargs):
+        if kwargs.get('ativo'):
+            raise ValueError('Não é possível sobrescrever o valor de ativo.')
+        return self.model.objects.filter(ativo=True, **kwargs)
 
-    def inativos(self):
-        return self.model.objects.filter(ativo=False)
+    def inativos(self, **kwargs):
+        if kwargs.get('ativo'):
+            raise ValueError('Não é possível sobrescrever o valor de ativo.')
+        return self.model.objects.filter(ativo=False, **kwargs)
+
